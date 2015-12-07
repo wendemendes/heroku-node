1596efba1030403438b02a6918ef9c91e38c850c
Wende Mendes <wende@bluesoft.com.br>
2015-12-07 15:26:04 -0200

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var server = http.createServer(function(req, res){
 	res.end('<h1>Hello World</h1>');
 });
 
-var port = Number(process.env.PORT | 3000);
+var port = Number(process.env.PORT || 3000);
 
 
-server.listen(port);
